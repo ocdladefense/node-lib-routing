32fc415f92191fcc401bd36db671dd77eb37abb0
José Bernal <jbernal.web.dev@gmail.com>
2024-10-14 15:02:00 -0700

    Provide match() method that will return a best match route for a given path.

--- src/Router.js
+++ src/Router.js
@@ -1,16 +1,23 @@
-/** @jsx vNode */ /** @jsxFrag "Fragment" */
-import { vNode, View } from "@ocdla/view";
+
 
 /**
  * @classdesc Router class to handle routing logic for a web application.
  */
 class Router {
 
+
+    basePath = "/";
+
+    /**
+     * The default path to look for when loading components.
+     */
+    static componentPath = "/src/components";
+
     /**
      * @private
      * @type {Object}
      */
-    #routes;
+    #routes = [];
 
     /**
      * @private
@@ -23,8 +30,56 @@ class Router {
      * @constructor
      * @param {Object} routes - An object mapping route names to page components.
      */
+
+    #notFoundCallback;
+
+
     constructor(routes) {
-        this.#routes = routes;
+        this.#routes = routes || [];
+    }
+
+
+    setComponentPath(path) {
+        Router.componentPath = path;
+    }
+
+    setNotFoundCallback(callback) {
+        this.#notFoundCallback = callback;
+    }
+
+
+    match(_path) {
+        // Leave the root path alone; compensate for any trailing slashes.
+        const normalized = _path === '/' ? '/' : _path.replace(/\/+$/, '');
+        const parts = normalized.split('/');
+        const _var = parts.length > 2 ? parts[parts.length - 2] : null;
+
+        for (const r of this.#routes.reverse()) {
+            let { path, callback, params } = r;
+
+            path = path.replaceAll('/', '\\/');
+
+            // May need to add in modifiers / flags.
+            const re = new RegExp(path);
+            const matches = _path.match(re);
+
+            // If matches is null, then this didn't match.
+            if(!matches) continue;
+
+            if (null !== _var) {
+                params[_var] = matches[1];
+            }
+
+            return [callback, params];
+        }
+
+        return [this.#notFoundCallback, {}];
+    }
+
+
+
+    setBasePath(path) {
+        this.basePath = path;
     }
 
     /**
@@ -43,23 +98,20 @@ class Router {
         this.#defaultPage = page;
     }
 
-    /**
-     * Gets the page component for the current route.
-     * @returns {Promise<*>} A promise that resolves to the page component.
-     */
-    async getPage() {
-        const location = window.location.href.split("/").at(-1).toLowerCase();
         
-        // Module providing the component.
-        let module;
 
-        // Component that should be imported and then injected into the page.
-        let page = this.#routes[location] || this.#defaultPage;
 
-        module = await import(`../../../../src/components/pages/${page}`);
+    addRoute(path, callback, params = {}) {
+        const routeExists = this.#routes.find(r => r.route === path);
 
-        return module.default;
+        if (routeExists) {
+            routeExists.id = id;
+            routeExists.callback = callback;
+        } else this.#routes.push({ path, callback, params });
     }
+
+
+
 
     /**
      * Gets the current route from the URL.
